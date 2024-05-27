b745b85d9dbb12938b88ad088c00d565259b36ac
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-05-27 10:26:12 +0900

    [Silver III] Title: 크리스마스 선물, Time: 412 ms, Memory: 40920 KB -BaekjoonHub

---
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -0,0 +1,17 @@
+import heapq
+n = int(input())
+
+lst = []
+
+for _ in range(n):
+    a = input()
+    if a == '0':
+        if len(lst) == 0:
+            print(-1)
+        else:
+            print(-heapq.heappop(lst))
+        continue
+    else:
+        k = list(map(int,a.split()))[1:]
+        for i in k:
+            heapq.heappush(lst,-i)
