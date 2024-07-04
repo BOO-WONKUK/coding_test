89652687e82f4ce825d6ed73b3e039df4a3cda5c
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-07-05 01:25:36 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,8 @@
+import math
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n)) # combination 공식을 factorial로 구했다.
+    print(bridge)
