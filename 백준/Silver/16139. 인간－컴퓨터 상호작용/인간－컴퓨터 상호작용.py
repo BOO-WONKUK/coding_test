d7ec6817fc7d1fa20582e06f13bd4a81dd70904b
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-08-24 21:42:02 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 88 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,24 @@
+s = input()
+n = int(input())
+lst = []
+for _ in range(n):
+    lst.append(list(input().split()))
+
+tmp = lst[0][0]
+prefixsums = {chr(i): [0] * (len(s) + 1) for i in range(ord('a'), ord('z') + 1)}
+
+for i in range(len(s)):
+    for alpha in prefixsums:
+        if s[i] == alpha:
+            prefixsums[alpha][i + 1] = prefixsums[alpha][i] + 1
+        else:
+            prefixsums[alpha][i + 1] = prefixsums[alpha][i]
+
+for j in range(n):
+    a, b, c = lst[j]
+    l = int(b)
+    r = int(c)
+    if l == 0:
+        print(prefixsums[a][r + 1])
+    else:
+        print(prefixsums[a][r + 1] - prefixsums[a][l])
