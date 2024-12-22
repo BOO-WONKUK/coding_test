934b84b5da03e19799ba6fa79dd34695520faaf4
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-12-22 21:20:33 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for a,b,c in commands:
+        tmp = array[a-1:b]
+        tmp.sort()
+        answer.append(tmp[c-1])
+        
+    return answer
