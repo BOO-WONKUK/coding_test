20eb4837a7e22a508081b9f270f62f6e1a3e053f
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2025-03-05 22:22:16 +0900

    [Silver V] Title: 행렬 곱셈, Time: 172 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -0,0 +1,12 @@
+n,m = map(int, input().split())
+
+A = [list(map(int,input().split())) for _ in range(n)]
+m, k = map(int,input().split())
+B = [list(map(int,input().split())) for _ in range(m)]
+C = [list(0 for _ in range(k)) for _ in range(n)]
+for i in range(n):
+    for j in range(k):
+        for l in range(m):
+            C[i][j] += A[i][l] * B[l][j]
+for r in C:
+    print(*r)
