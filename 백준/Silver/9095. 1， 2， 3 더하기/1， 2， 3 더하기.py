2f0b3790ecc0f4146126767b8e6ff34975850893
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2025-03-19 21:20:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,15 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    lst.append(int(input()))
+    
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4,11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    
+for i in lst:
+    print(dp[i])
